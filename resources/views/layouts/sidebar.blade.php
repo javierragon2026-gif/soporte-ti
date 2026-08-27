be98c434d6f98efab261a6be9176f7aed877b715
Diana Cordova <dcord@ragon.com.mx>
2026-08-27 09:08:37 -0600

    login

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -33,7 +33,7 @@
             'url' => route('teams.index'),
             'title' => trans_choice('team.team', 2),
         ])
-        @if (auth()->user()->admin)
+        @if (auth()->user()?->admin)
             @include('components.sidebarItem', [
                 'url' => route('users.index'),
                 'title' => trans_choice('ticket.user', 2),
